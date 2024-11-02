fbb9f0a53ec845e1d38b1c4b8ccf38094d52a5e3
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-02 23:37:11 +0100

    Initial Commit

---
+++ MyHelloCoffee/MyHelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHelloCoffeeApp.swift
+//  MyHelloCoffee
+//
+//  Created by Jean on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
